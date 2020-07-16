a3ce685b1cb63a35bb1c985029f4bc3303b415bb
BharathParvatham <bharath.parvatham@msitprogram.net>
2020-07-16 19:11:45 +0530

    Done

--- 10-issorted-Python/issorted.py
+++ 10-issorted-Python/issorted.py
@@ -1,10 +1,35 @@
 # isSorted(a) (10 Pts)
-# Write the function isSorted(a) that takes a list of numbers and returns True if the list 
-# is sorted (either smallest-first or largest-first) and False otherwise. Your function 
-# must only consider each value in the list once (so, in terms of big-oh, which we will 
-# learn soon, it runs in O(n) time, where n=len(a)), and so in particular you may not sort 
+# Write the function isSorted(a) that takes a list of numbers and returns True if the list
+# is sorted (either smallest-first or largest-first) and False otherwise. Your function
+# must only consider each value in the list once (so, in terms of big-oh, which we will
+# learn soon, it runs in O(n) time, where n=len(a)), and so in particular you may not sort
 # the list.
 
+def lessThan(arr):
+    for index in range(len(arr) - 1):
+        if arr[index] > arr[index + 1]:
+            return False
+    return True
+
+
+def greaterThan(arr):
+    for index in range(len(arr) - 1):
+        if arr[index] < arr[index + 1]:
+            return False
+    return True
+
+
 def issorted(a):
-	# your code goes here
-	pass
+    # your code goes here
+    if len(a) == 0 or len(a) == 1:
+        return True
+    index = 0
+
+    while(index + 1 < len(a)):
+        if a[index] == a[index + 1]:
+            index += 1
+        elif a[index] < a[index + 1]:
+            return lessThan(a[index:])
+        else:
+            return greaterThan(a[index:])
+    return True
