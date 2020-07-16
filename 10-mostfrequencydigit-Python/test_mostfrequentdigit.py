42b5d1ffeda69adb01bda562e82c092e4aa3ed1d
BharathParvatham <bharath.parvatham@msitprogram.net>
2020-07-16 10:40:04 +0530

    Jul 16  10:40:04

--- 10-mostfrequencydigit-Python/test_mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/test_mostfrequentdigit.py
@@ -1,19 +1,20 @@
-import os,sys
-sys.path.append(os.getcwd())
-from mostfrequentdigit import mostfrequentdigit
 import pytest
+from mostfrequentdigit import mostfrequentdigit
+import os
+import sys
+sys.path.append(os.getcwd())
 
 
-@pytest.mark.parametrize('n, result',[
-	(24, 2),
-	(0, 0),
-	(26011, 1),
-	(14, 1),
-	(2, 2),
-	(5, 5),
-	(5231123123123, 1),
-	(5312312355565, 5),
-	(1102300, 0),
+@pytest.mark.parametrize('n, result', [
+    (24, 2),
+    (0, 0),
+    (26011, 1),
+    (14, 1),
+    (2, 2),
+    (5, 5),
+    (5231123123123, 1),
+    (5312312355565, 5),
+    (1102300, 0),
 ])
 def test_mostfrequentdigit(n, result):
     assert mostfrequentdigit(n) == result
