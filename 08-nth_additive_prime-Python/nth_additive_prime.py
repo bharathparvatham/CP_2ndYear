2a5c6e056d9b27dacffa58a802a85489f8d8cd56
BharathParvatham <bharath.parvatham@msitprogram.net>
2020-07-17 23:15:08 +0530

    Done

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -1,10 +1,47 @@
-# Write the function fun_nth_additive_prime(n) that takes a non-negative int n 
-# and returns the nth Additive Prime, which is a prime number such that 
-# the sum of its digits is also prime. For example, 113 is prime and 1+1+3==5 and 5 
+# Write the function fun_nth_additive_prime(n) that takes a non-negative int n
+# and returns the nth Additive Prime, which is a prime number such that
+# the sum of its digits is also prime. For example, 113 is prime and 1+1+3==5 and 5
 # is also prime, so 113 is an Additive Prime. fun_nth_additive_prime(0) returns 2
 
+import math
 
+
+def isPrime(number):
+    for index in range(2, math.floor(number ** 0.5) + 1):
+        if number % index == 0:
+            return False
+    return True
+
+
+def primesInRange(start, end):
+    primes = []
+    for num in range(start, end + 1):
+        if isPrime(num):
+            primes.append(num)
+    return primes
+
+
+def sumOfDigits(number):
+    result = 0
+    while number > 0:
+        result += number % 10
+        number = number // 10
+    return result
 
 
 def fun_nth_additive_prime(n):
-	return 1
+    additivePrimes = [2, 3, 5, 7]
+
+    if n < len(additivePrimes) - 1:
+        return additivePrimes[n]
+    else:
+        start = 8
+        while len(additivePrimes) - 1 < n:
+            primes = primesInRange(start, start + 20)
+            for prime in primes:
+                if isPrime(sumOfDigits(prime)):
+                    additivePrimes.append(prime)
+                    if len(additivePrimes) - 1 == n:
+                        break
+            start += 21
+        return additivePrimes[n]
