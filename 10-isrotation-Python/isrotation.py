a94131fec619db70609f10c515f5dc3216275eac
BharathParvatham <bharath.parvatham@msitprogram.net>
2020-07-17 23:58:36 +0530

    Jul 17  23:58:35

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -1,10 +1,21 @@
 # isRotation(x, y) [15 pts]
-# Write the function isRotation(x, y) that takes two non-negative integers x and y, both 
-# guaranteed to not contain any 0's, and 
-# returns True if x is a rotation of the digits of y and False otherwise. For example, 
-# 3412 is a rotation of 1234. Any number 
+# Write the function isRotation(x, y) that takes two non-negative integers x and y, both
+# guaranteed to not contain any 0's, and
+# returns True if x is a rotation of the digits of y and False otherwise. For example,
+# 3412 is a rotation of 1234. Any number
 # is a rotation of itself.
 
 def isrotation(x, y):
-	# Your code goes here
-	pass
+    # Your code goes here
+    x = str(x)
+    y = str(y)
+
+    if len(x) != len(y):
+        return False
+
+    y = y + y
+
+    if x in y:
+        return True
+
+    return False
