1b37ee8f21bc4311ad7ce65515002316925d413c
BharathParvatham <bharath.parvatham@msitprogram.net>
2020-07-20 12:31:13 +0530

    Jul 20  12:31:13

--- 02-hasnoprimes-Python/hasnoprimes.py
+++ 02-hasnoprimes-Python/hasnoprimes.py
@@ -1,7 +1,11 @@
-# Write the function hasnoprimes(L) that takes a 2d list L of integers, 
+# Write the function hasnoprimes(L) that takes a 2d list L of integers,
 # and returns True if L does not contain any primes, and False otherwise.
 
 
 def fun_hasnoprimes(l):
-	return True
-
+    for numList in l:
+        for num in numList:
+            if isPrime(num):
+                return False
+    else:
+        return True
