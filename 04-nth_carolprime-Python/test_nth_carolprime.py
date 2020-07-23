b2bfaf4ab69f4086fda184fbbdfabf44ec5ef820
BharathParvatham <bharath.parvatham@msitprogram.net>
2020-07-23 22:48:48 +0530

    Jul 23  22:48:48

--- 04-nth_carolprime-Python/test_nth_carolprime.py
+++ 04-nth_carolprime-Python/test_nth_carolprime.py
@@ -1,14 +1,14 @@
-import os,sys
-sys.path.append(os.getcwd())
-from nth_carolprime import fun_nth_carolprime
 import pytest
+from nth_carolprime import fun_nth_carolprime
+import os
+import sys
+sys.path.append(os.getcwd())
 
 
-@pytest.mark.parametrize('a, result',[
-   (0, 7),(1, 47),(2, 223),(3, 959),
-(4,3967),(5, 16127), (6, 65023)
+@pytest.mark.parametrize('a, result', [
+    (0, 7), (1, 47), (2, 223), (3, 3967),
+    (4, 16127), (5, 261119), (6, 1046527)
 
 ])
 def test_fun_nth_carolprime(a, result):
     assert fun_nth_carolprime(a) == result
-
